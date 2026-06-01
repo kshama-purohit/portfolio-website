7b83af726f3892a78c5a1ab9c71b84e7f699ae1d
kshamapurohit <kshamapurohit29@gmail.com>
2026-05-31 20:45:01 -0400

    add csrf trusted origins

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://web-production-9d05bb.up.railway.app']
